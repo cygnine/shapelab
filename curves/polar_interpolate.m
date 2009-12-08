a5bc24b32b34322427080a6a4257858ce7fc5d0f
Akil Narayan <anaray@dam.brown.edu>
2009-12-08 17:40:23 -0500

    Fixed bug in polar interpolation scheme

--- curves/polar_interpolate.m
+++ curves/polar_interpolate.m
@@ -4,11 +4,22 @@ function[z] = polar_interpolate(z1, z2, N)
 % z = polar_interpolate(z1, z2, N)
 % 
 %     Interpolates the points z1 and z2 using a 'linspace' in polar coordinates,
-%     meaning that the angles and radii of the output z are linearly spaced.
+%     meaning that the angles and radii of the output z are linearly spaced. It
+%     does this in the 'shortest' way possible in the sense that it picks the
+%     angular path with angular change no greater than pi.
 
 r1 = abs(z1);
 r2 = abs(z2);
 a1 = mod(angle(z1), 2*pi);
 a2 = mod(angle(z2), 2*pi);
 
-z = linspace(r1, r2, N).*exp(i*linspace(a1, a2, N));
+if abs(a1-a2)<=pi
+  z = linspace(r1, r2, N).*exp(i*linspace(a1, a2, N));
+else
+  if a2-a1>0
+    a2 = a2 - 2*pi;
+  else
+    a2 = a2 + 2*pi;
+  end
+  z = linspace(r1, r2, N).*exp(i*linspace(a1, a2, N));
+end
